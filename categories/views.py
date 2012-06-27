45eaac1c467cfefe42075dec40d039e371a20fc9
Saumil Dalal <dalal.saumil@gmail.com>
2012-06-27 18:08:23 +0530

    Modified category admin

--- categories/views.py
+++ categories/views.py
@@ -3,11 +3,11 @@
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-
+from django.contrib.auth.decorators import login_required
 from categories.models import Category, CategoryGraph
 from categories.forms import *
 
-
+@login_required
 def view_all_categories(request):
     cat = Category.objects.all()
     q = request.GET.get('q')
@@ -16,10 +16,11 @@ def view_all_categories(request):
     cat_dict = {    
         'categories': cat,
         'q':q,
+        'count': cat.count(),
         }    
-    return render_to_response('categories/view_all_categories.html', cat_dict, context_instance=RequestContext(request))
+    return render_to_response('riba-admin/categories/view_all_categories.html', cat_dict, context_instance=RequestContext(request))
 
-
+@login_required
 def add_category(request, id=None):
     form = CategoryForm()
     errors = []
@@ -28,7 +29,7 @@ def add_category(request, id=None):
             form = CategoryForm(request.POST, request.FILES)
             if form.is_valid():
                 q = form.save()
-                return HttpResponseRedirect('/categories/%s' %q.id)
+                return HttpResponseRedirect('/admin/categories/%s' %q.id)
             else:
                 for er in form.errors:
                     errors.append(form.errors[er])
@@ -36,17 +37,17 @@ def add_category(request, id=None):
         'form': form,
         'errors': errors,
     }
-    return render_to_response('categories/add_category.html', ctxt, context_instance=RequestContext(request))
+    return render_to_response('riba-admin/categories/add_category.html', ctxt, context_instance=RequestContext(request))
 
-
+@login_required
 def view_category(request, id):
     q = Category.objects.get(pk=id)
     ctxt = {
         'category': q,
     }
-    return render_to_response('categories/category.html', ctxt, context_instance=RequestContext(request))
+    return render_to_response('riba-admin/categories/category.html', ctxt, context_instance=RequestContext(request))
 
-
+@login_required
 def edit_category(request, id):
     errors = []
     cat = Category.objects.get(pk=id)
@@ -55,7 +56,7 @@ def edit_category(request, id):
         form = CategoryForm(request.POST, request.FILES, instance=cat)
         if form.is_valid():
             cat = form.save()
-            return HttpResponseRedirect('/categories/%s' %cat.id)
+            return HttpResponseRedirect('/admin/categories/%s' %cat.id)
         else:
             for er in form.errors:
                 errors.append(form.errors[er])
@@ -64,8 +65,9 @@ def edit_category(request, id):
         'form': form,
         'errors': errors
     }
-    return render_to_response('categories/edit_category.html', ctxt, context_instance=RequestContext(request))
+    return render_to_response('riba-admin/categories/edit_category.html', ctxt, context_instance=RequestContext(request))
 
+@login_required
 def delete_category(request, id):
     try:
         category = Category.objects.get(id = id)
@@ -75,9 +77,9 @@ def delete_category(request, id):
         del_confirm = request.POST.get('del_confirm', 'No')
         if del_confirm == "Yes":
             category.delete()
-        return HttpResponseRedirect('/categories/view/')
+        return HttpResponseRedirect('/admin/categories/view/')
         
     ctxt = {
         'category':category,
     }    
-    return render_to_response('categories/category_delete_confirm.html', ctxt, context_instance=RequestContext(request))
+    return render_to_response('riba-admin/categories/category_delete_confirm.html', ctxt, context_instance=RequestContext(request))
