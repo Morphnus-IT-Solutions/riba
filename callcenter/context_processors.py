3502e12931ee1957fc95e51cc7002048451f467d
Saumil Dalal <dalal.saumil@gmail.com>
2012-08-20 23:27:11 +0530

    Removed callcenter app

--- callcenter/context_processors.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-
-def call(request):
-    call = getattr(request, 'call', None)
-    return dict(request_call=call)
-
-def conf(request):
-    conf = {'DEBUG': settings.DEBUG, 'MEDIA_PREFIX': settings.MEDIA_PREFIX}
-    return dict(conf=conf)
--- callcenter/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-handler500 = 'web.views.errors.handle505'
-handler404 = 'web.views.errors.handle404'
-
-urlpatterns = patterns('', 
-    (r'^[0-9.-]+/', include('callcenter.cc_urls')),
-    (r'', include('callcenter.cc_urls')),
-)
