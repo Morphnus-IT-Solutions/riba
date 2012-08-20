4b72069331ff4220d74592435163a1764eaa1541
Saumil Dalal <dalal.saumil@gmail.com>
2012-08-20 23:11:04 +0530

    Removed unwanted files

--- chmsettings.py
+++
@@ -1,7 +0,0 @@
-from settings import *
-
-TEMPLATE_DIRS = (
-    os.path.join(SETTINGS_FILE_FOLDER, 'templates/chmobile')
-)
-
-MEDIA_ROOT = os.path.join(SETTINGS_FILE_FOLDER, 'media/chmobile')
--- cssettinngs.py
+++
@@ -1,5 +0,0 @@
-from settings import *
-DEBUG = True
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(SETTINGS_FILE_FOLDER, 'media/cs')
