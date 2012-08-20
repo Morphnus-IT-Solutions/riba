16d0e53b97c70b96d3c7d54aa3ae23f3c70bd97a
Saumil Dalal <dalal.saumil@gmail.com>
2012-08-20 23:05:13 +0530

    Cleaned api app

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,1 @@
 from django.conf.urls.defaults import *
-import bl.api
-urlpatterns = patterns('',
-    (r'response/call/(?P<response_id>\d+)', 'api.views.call_response'),
-    (r'call/close', 'api.views.close_inbound_call'),
-    (r'attempt/(?P<attempt_id>\d+)/update', 'api.views.update_attempt'),
-    (r'attempt/(?P<attempt_id>\d+)', 'api.views.attempt_detail'),
-    (r'event/order_confirmed_get', 'api.views.order_confirmed_get'),
-    (r'event/order_confirmed', 'api.views.order_confirmed'),
-    (r'response/add_or_get', 'api.views.get_or_create_response'),
-    (r'response/add', 'api.views.add_response'),
-)
